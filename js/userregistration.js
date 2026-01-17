98fc83f50b1273e9200ea6a22b861bdbe83c5082
Sara Redwan Shure <susured22@gmail.com>
2026-01-17 13:26:20 +0300

    Update userregistration.js

--- js/userregistration.js
+++ js/userregistration.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+6document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
 
   form.addEventListener("submit", function (event) {
